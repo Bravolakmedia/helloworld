7bfb67796ac667797133f7a3db3a4119753d7713
Olukosi-Ismaila-Bamidele <olakunlebravo@gmail.com>
2024-03-29 12:59:50 +0100

    I overwrite the smart contract and the deploy.ts to prepare for deployment

--- deploy.ts
+++ deploy.ts
@@ -1,54 +1,24 @@
 import { Helloworld } from './src/contracts/helloworld'
-import {
-    bsv,
-    TestWallet,
-    DefaultProvider,
-    sha256,
-    toByteString,
-} from 'scrypt-ts'
+import { getDefaultSigner } from './tests/utils/txHelper'
+import { toByteString, sha256 } from 'scrypt-ts'
 
-import * as dotenv from 'dotenv'
+;(async () => {
+    // set network env
+    process.env.NETWORK = 'testnet'
 
-// Load the .env file
-dotenv.config()
+    const message = toByteString('hello world', true)
 
-if (!process.env.PRIVATE_KEY) {
-    throw new Error(
-        'No "PRIVATE_KEY" found in .env, Please run "npm run genprivkey" to generate a private key'
-    )
-}
-
-// Read the private key from the .env file.
-// The default private key inside the .env file is meant to be used for the Bitcoin testnet.
-// See https://scrypt.io/docs/bitcoin-basics/bsv/#private-keys
-const privateKey = bsv.PrivateKey.fromWIF(process.env.PRIVATE_KEY || '')
-
-// Prepare signer.
-// See https://scrypt.io/docs/how-to-deploy-and-call-a-contract/#prepare-a-signer-and-provider
-const signer = new TestWallet(
-    privateKey,
-    new DefaultProvider({
-        network: bsv.Networks.testnet,
-    })
-)
-
-async function main() {
     await Helloworld.loadArtifact()
+    const instance = new Helloworld(sha256(message))
 
-    // TODO: Adjust the amount of satoshis locked in the smart contract:
-    const amount = 1
+    // connect to a signer
+    await instance.connect(getDefaultSigner())
 
-    const instance = new Helloworld(
-        // TODO: Adjust constructor parameter values:
-        sha256(toByteString('hello world', true))
-    )
+    // deploy the contract and lock up 42 satoshis in it
+    const deployTx = await instance.deploy(42)
+    console.log('Helloworld contract deployed: ', deployTx.id)
 
-    // Connect to a signer.
-    await instance.connect(signer)
-
-    // Contract deployment.
-    const deployTx = await instance.deploy(amount)
-    console.log(`Helloworld contract deployed: ${deployTx.id}`)
-}
-
-main()
+    // contract call
+    const { tx: callTx } = await instance.methods.unlock(message)
+    console.log('Helloworld contract `unlock` called: ', callTx.id)
+})()
