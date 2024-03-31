acff0b5ebcacdccf72f38e6cd4e2d1f274f723d7
Olukosi-Ismaila-Bamidele <olakunlebravo@gmail.com>
2024-03-31 21:28:38 +0100

    I integrated the helloworld smart contract with the hello react front end

--- hello/src/index.tsx
+++ hello/src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Helloworld } from "./contracts/helloworld";
+import artifact from "../artifacts/helloworld.json";
+Helloworld.loadArtifact(artifact);
+const message = toByteString("hello world", true);
+const instance = new Helloworld(sha256(message));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,3 +22,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const provider = new DefaultProvider({
+  network: bsv.Networks.testnet,
+});
